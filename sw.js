cb1b23404498ffad2415dacdcc68af833cb5674a
uraro <uraguchitakamitsu@gmail.com>
2026-02-28 02:59:28 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,22 +1,10 @@
-const CACHE_NAME = 'calc-v1';
-const ASSETS = [
-  './',
-  './index.html',
-  'https://fonts.googleapis.com/css2?family=M+PLUS+Rounded+1c:wght@300&display=swap'
-];
+const CACHE_NAME = 'calc-v2';
+const urlsToCache = [ './', './index.html', './manifest.json' ];
 
-// インストール時にファイルをキャッシュ
 self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
-  );
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache)));
 });
 
-// オフライン時はキャッシュから応答
 self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
+  event.respondWith(caches.match(event.request).then((response) => response || fetch(event.request)));
 });
